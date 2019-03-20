a3e5a7f4e7bcaea1b545f5a8e435f6b9b99f0a3e
PODS4012 <pods4012@gmail.com>
2019-03-20 23:23:09 +0100

    finnished adding the classes

--- Csharp_WhileLoopAndComplex/NumberGuesser.cs
+++ Csharp_WhileLoopAndComplex/NumberGuesser.cs
@@ -48,6 +48,8 @@ namespace Csharp_WhileLoopAndComplex
 
         #endregion
 
+        #region Public Methods
+
         /// <summary>
         /// Asks the user to think of a number between 0 and the maximum number
         /// </summary>
@@ -57,5 +59,94 @@ namespace Csharp_WhileLoopAndComplex
             Console.WriteLine($"I want you to think of a number between 0 an { this.MaximumNumber }. OK?");
             Console.ReadLine();
         }
+
+        /// <summary>
+        /// Ask the user a series of questions to discover the number they are thinking of
+        /// </summary>
+        public void DiscoverNumber()
+        {
+            // Clear variables to thier initial values before a discovery
+            this.CurrentNumberOfGuesses = 0;
+            this.CurrentGuessMinimum = 0;
+            this.CurrentGuessMaximum = this.MaximumNumber / 2;
+
+            // While the guess isn't the same as the known maximum value
+            while (this.CurrentGuessMinimum != this.CurrentGuessMaximum)
+            {
+
+                // Incrase guess amount (by 1)
+                this.CurrentNumberOfGuesses++;
+
+                // Ask the user if thier number is between the guess range
+                Console.WriteLine($"Is your number between { this.CurrentGuessMinimum } and { this.CurrentGuessMaximum }?");
+                string response = Console.ReadLine();
+
+                // If the user confirmed thier number is within the current range...
+                //bool hasResponse = (response != null && response.Length > 0);
+                //if (hasResponse && (response[0] == 'y' || response[0] == 'Y'))
+
+                if (response?.ToLower().FirstOrDefault() == 'y')
+                {
+                    // We know the number is between guessTFrom and guessTo
+                    // So set the new maximum number
+                    this.MaximumNumber = this.CurrentGuessMaximum;
+
+                    // Change the next guess range to be half of the maximum range
+                    this.CurrentGuessMaximum = this.CurrentGuessMaximum - (this.CurrentGuessMaximum - this.CurrentGuessMinimum) / 2;
+                }
+                // The number is greater than guessMax and less than or equal to max
+                else
+                {
+                    // The new minimum is one above the old maximum
+                    this.CurrentGuessMinimum = this.CurrentGuessMaximum + 1;
+
+                    // Guess the bottom half of the new range
+                    int remainingDifferance = this.MaximumNumber - this.CurrentGuessMaximum;
+
+                    // Set the guess max to half way between the guessMin and max
+                    // NOTE: Math.Ceiling will round up the remaining difference to 2, if the difference is 1,1
+                    this.CurrentGuessMaximum += (int)Math.Ceiling(remainingDifferance / 2f);
+                }
+
+                // if we only have 2 numbers left, guess one of them
+                if (this.CurrentGuessMinimum + 1 == this.MaximumNumber)
+                {
+                    // Incrase guess amount (by 1)
+                    this.CurrentNumberOfGuesses++;
+
+                    // Ask the user if thier number is the lower number
+                    Console.WriteLine($"Is your number { this.CurrentGuessMinimum }?");
+                    response = Console.ReadLine();
+
+                    //if the user  confirmed thier number is the lower number...
+                    if (response?.ToLower().FirstOrDefault() == 'y')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // That means the number must be higher of the two
+                        this.CurrentGuessMinimum = this.MaximumNumber;
+                        break;
+                    }
+                }
+            } // End of while (guessMin != max)
+
+        }
+
+        /// <summary>
+        /// Announces the number the user was thinking of and the number of guesses it took 
+        /// </summary>
+        public void AnnounceResults()
+        {
+            // Tell the user thier number
+            Console.WriteLine($"** Your number is { this.CurrentGuessMinimum } **");
+
+            // Tell the user how many guessys it took
+            Console.WriteLine($"Guessed in { this.CurrentNumberOfGuesses } guesses");
+            Console.ReadLine();
+        }
+
+        #endregion
     }
 }
--- Csharp_WhileLoopAndComplex/Program.cs
+++ Csharp_WhileLoopAndComplex/Program.cs
@@ -14,79 +14,16 @@ namespace Csharp_WhileLoopAndComplex
             var numberGuesser = new NumberGuesser();
 
             // Change the default maximum number to 200
-            numberGuesser.MaximumNumber = 200;
+            numberGuesser.MaximumNumber = 100;
 
             // Ask the user to think of a number
             numberGuesser.InformUser();
 
+            // Discover the number the user is thinking of
+            numberGuesser.DiscoverNumber();
 
-
-            while (guessMin != max)
-            {
-
-                // Incrase guess amount (by 1)
-                guesses++;
-
-                // Ask the user if thier number is between the guess range
-                Console.WriteLine($"Is your number between { guessMin } and { guessMax }?");
-                string response = Console.ReadLine();
-
-                // If the user confirmed thier number is within the current range...
-                //bool hasResponse = (response != null && response.Length > 0);
-                //if (hasResponse && (response[0] == 'y' || response[0] == 'Y'))
-
-                if (response?.ToLower().FirstOrDefault() == 'y')
-                {
-                    // We know the number is between guessTFrom and guessTo
-                    // So set the new maximum number
-                    max = guessMax;
-
-                    // Change the next guess range to be half of the maximum range
-                    guessMax = guessMax - (guessMax - guessMin) / 2;
-                }
-                // The number is greater than guessMax and less than or equal to max
-                else
-                {
-                    // The new minimum is one above the old maximum
-                    guessMin = guessMax + 1;
-
-                    // Guess the bottom half of the new range
-                    int remainingDifferance = max - guessMax;
-
-                    // Set the guess max to half way between the guessMin and max
-                    // NOTE: Math.Ceiling will round up the remaining difference to 2, if the difference is 1,1
-                    guessMax += (int)Math.Ceiling(remainingDifferance / 2f);
-                }
-
-                // if we only have 2 numbers left, guess one of them
-                if (guessMin + 1 == max)
-                {
-                    // Incrase guess amount (by 1)
-                    guesses++;
-
-                    // Ask the user if thier number is the lower number
-                    Console.WriteLine($"Is your number { guessMin }?");
-                    response = Console.ReadLine();
-
-                    //if the user  confirmed thier number is the lower number...
-                    if (response?.ToLower().FirstOrDefault() == 'y')
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        // That means the number must be higher of the two
-                        guessMin = max;
-                        break;
-                    }
-                }
-            } // End of while (guessMin != max)
-            // Tell the user thier number
-            Console.WriteLine($"** Your number is { guessMin } **");
-
-            // Tell the user how many guessys it took
-            Console.WriteLine($"Guessed in { guesses } guesses");
-            Console.ReadLine();
+            // Announce the results
+            numberGuesser.AnnounceResults();
         }
     }
 }
